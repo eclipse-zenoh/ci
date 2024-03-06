6d25061c3135bb54b1f6b1c6d22665d0fb219065
Mahmoud Mazouz <mazouz.mahmoud@outlook.com>
2024-03-06 17:49:51 +0100

    fix: Always install target when cross compiling (#17)

--- src/build-crates-debian.ts
+++ src/build-crates-debian.ts
@@ -43,6 +43,8 @@ export async function main(input: Input) {
     const remote = `https://${input.githubToken}@github.com/${input.repo}.git`;
     sh(`git clone --recursive --branch ${input.branch} --single-branch ${remote}`);
 
+    sh(`rustup target add ${input.target}`);
+
     sh(`cross build --release --bins --lib --target ${input.target}`, {
       cwd: repo,
     });
