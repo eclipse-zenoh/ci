f7178f61ca29ad8ae00c52d2d816be225f0290ff
Mahmoud Mazouz <mazouz.mahmoud@outlook.com>
2024-03-08 12:08:01 +0100

    fix: Guard against creating empty ZIP archives

--- src/zip.ts
+++ src/zip.ts
@@ -14,6 +14,13 @@ export async function fromDirectory(output: string, dir: string, pattern: RegExp
 }
 
 export function fromFiles(output: string, ...files: string[]) {
+  if (files.length === 0) {
+    // NOTE: If the files array is empty, 7-Zip will scan the current directory
+    // for files and directories to add to the archive, while Info-ZIP will
+    // return a non-zero exit code
+    throw new Error("Attempt to create empty ZIP archive");
+  }
+
   const platform = os.platform();
   if (platform == "linux" || platform == "darwin") {
     sh(`zip --verbose --junk-paths ${output} ${files.join(" ")}`);
