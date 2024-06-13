d1b830607666762de18659d3bb551711c66ce032
Diogo Mendes Matsubara <diogo.matsubara@zettascale.tech>
2024-06-13 15:52:49 +0200

    another fix

--- src/publish-crates-debian.ts
+++ src/publish-crates-debian.ts
@@ -85,7 +85,7 @@ export async function main(input: Input) {
     sh("sudo apt-get update");
     sh("sudo apt-get install -y dpkg-dev");
 
-    await fs.writeFile(packagesPath, sh(`dpkg-scanpackages --multiversion ${input.version}`));
+    await fs.writeFile(packagesPath, sh(`dpkg-scanpackages --multiversion ${input.version}-stable`));
     const packages = sh("cat .Packages-*");
     // NOTE: An unzipped package index is necessary for apt-get to recognize the
     // local repository created below
