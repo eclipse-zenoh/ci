5aa004b7d57a77a8985e0312941c0bc3264c96f5
Mahmoud Mazouz <mazouz.mahmoud@outlook.com>
2024-06-20 15:23:39 +0200

    revert: Don't ignore workspace dev dependencies in `cargo.packages`
    This reverts commit c5d26dcd35a8bf56c4db322e1e644801882f31ce.

--- dist/bump-crates-main.js
+++ dist/bump-crates-main.js
@@ -81006,7 +81006,7 @@ function packages(path) {
             manifestPath: elem.manifest_path,
             publish: elem.publish == null ? undefined : false,
             workspaceDependencies: elem.dependencies
-                .filter(dep => "path" in dep && dep.kind !== "dev")
+                .filter(dep => "path" in dep)
                 .map(dep => ({
                 name: dep.name,
                 req: dep.req,
