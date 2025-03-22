d77c1028c8ac3cd48fa1ee8c5011a36b897df34d
Diogo Mendes Matsubara <diogo.matsubara@zettascale.tech>
2025-03-22 01:08:12 +0100

    fix: fix tsup to bundle node project

--- build.js
+++ build.js
@@ -7,7 +7,7 @@ async function main() {
   const dir = await fs.opendir("src");
   for await (const dirent of dir) {
     if (["-pre.ts", "-main.ts", "-post.ts"].some(x => dirent.name.endsWith(x))) {
-      child_process.exec(`tsup src/${dirent.name} --out-dir dist --format esm`, (error, stdout, stderr) => {
+      child_process.exec(`tsup-node src/${dirent.name} --out-dir dist --format esm`, (error, stdout, stderr) => {
         if (error) {
           console.error(`exec error: ${error}`);
           return;
