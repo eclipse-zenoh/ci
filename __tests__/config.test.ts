9b3d6f0facef8983971ca653bfac77252277f30c
Diogo Mendes Matsubara <diogo.matsubara@zettascale.tech>
2025-05-26 14:24:50 +0200

    fix: add test for config.ts

---
+++ __tests__/config.test.ts
@@ -0,0 +1,34 @@
+import { describe, expect, test, jest } from "@jest/globals";
+
+import { config, gitEnv } from "../src/config";
+
+describe("default config", () => {
+  test("default config", async () => {
+    expect(config.git.user.name).toBe("eclipse-zenoh-bot");
+    expect(config.git.user.email).toBe("eclipse-zenoh-bot@users.noreply.github.com");
+  });
+
+  test("default gitEnv", async () => {
+    expect(gitEnv["GIT_AUTHOR_NAME"]).toBe("eclipse-zenoh-bot");
+    expect(gitEnv["GIT_AUTHOR_EMAIL"]).toBe("eclipse-zenoh-bot@users.noreply.github.com");
+  });
+});
+
+describe("overriden config", () => {
+  let overridenConfig: typeof import("../src/config");
+
+  beforeEach(() => {
+    jest.resetModules();
+    // Set environment variables for testing
+    process.env.GITHUB_AUTHOR_NAME = "foobar";
+    process.env.GITHUB_AUTHOR_EMAIL = "foobar@example.com";
+    return import("../src/config").then(module => {
+      overridenConfig = module;
+    });
+  });
+
+  test("overriden gitEnv", async () => {
+    expect(overridenConfig.gitEnv["GIT_AUTHOR_NAME"]).toBe("foobar");
+    expect(overridenConfig.gitEnv["GIT_AUTHOR_EMAIL"]).toBe("foobar@example.com");
+  });
+});
